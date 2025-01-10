74e9dfb079147b08ee6d6b1ab9929375e2327f2d
vitor <vitorbittencourttt05@gmail.com>
2025-01-09 22:09:29 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/alura/screenmatchh/model/DadosSerie.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatchh.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String title,
+                         @JsonAlias("Runtime") String totalTempFilme,
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonProperty("imdbVotes") String votos) {
+
+
+ }
